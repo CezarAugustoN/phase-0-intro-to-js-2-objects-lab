cef65fcf74380e2e23488193baa49fceb7da4764
CezarAugustoNeves <creatingafuture94@gmail.com>
2023-03-19 06:33:21 -0400

    adds

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee ={
+    name : "Sam"
+}
+function updateEmployeeWithKeyAndValue (employee, name, value){
+    const newEmployee ={...employee};
+    newEmployee[name] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, name, value){
+    employee[name] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey (employee, key){
+    const newEmployee = {employee};
+    delete newEmployee[key];
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee) {
+    delete employee.name;
+    return employee;
+}
